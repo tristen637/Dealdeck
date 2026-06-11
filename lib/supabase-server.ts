10938fd43db1b3ee3cfe14fa476dde31048b0065
tristen637 <tbrendel2006@gmail.com>
2026-06-11 15:39:06 -0500

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -22,3 +22,9 @@ export function createServerSupabase() {
 }
 
 export function createAdminSupabase() {
+  const { createClient } = require('@supabase/supabase-js')
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  )
+}
